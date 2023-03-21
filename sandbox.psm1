abca8d46776a93692a899e188a6f17f93c0af022
Luke Davidson <luked@yvfwc.org>
2023-03-20 23:09:47 -0700

    Set features in a hashtable rather than switches

--- sandbox.psm1
+++ sandbox.psm1
@@ -1,10 +1,12 @@
 <#PSScriptInfo
-.VERSION 2.0
-.AUTHOR luke@lukedavidson.org
-.COPYRIGHT 2023 Luke Davidson. All rights reserved.
-.LICENSEURI https://github.com/lukecdavidson/sandbox/blob/master/LICENSE
-.PROJECTURI https://github.com/lukecdavidson/sandbox
+    .VERSION 2.0
+    .AUTHOR luke@lukedavidson.org
+    .COPYRIGHT 2023 Luke Davidson. All rights reserved.
+    .LICENSEURI https://github.com/lukecdavidson/sandbox/blob/master/LICENSE
+    .PROJECTURI https://github.com/lukecdavidson/sandbox
 #>
+
+
 function Approve-VolumesMap {
     param([hashtable[]]$Volumes)
 
@@ -25,104 +27,85 @@ function Approve-VolumesMap {
     return $true
 }
 
+
 function Start-Sandbox {
     <#
-    .SYNOPSIS
-    Starts Windows Sandbox
-    .DESCRIPTION
-    PowerShell wrapper that builds the configuration XML for Windows Sandbox and executes.
-    .EXAMPLE
-    Start-Sandbox -Volume @{'Host'=$pwd; 'Container'='C:\Data'; 'Write'=$true}, @{'Host'='D:\mycode'; 'Container'='C:\mycode'} -Networking $false
+        .SYNOPSIS
+        Starts Windows Sandbox
+        .DESCRIPTION
+        PowerShell wrapper that builds the configuration XML for Windows Sandbox and executes.
+        .EXAMPLE
+        Start-Sandbox -Volume @{'Host'=$pwd; 'Container'='C:\Data'; 'Write'=$true}, @{'Host'='D:\mycode'; 'Container'='C:\mycode'} -Networking $false
     #>
     
     [CmdletBinding(DefaultParameterSetName='x')]
     Param (
+        # Specifies a path to an input WSB file which is run by WindowsSandbox.exe
+        [Parameter(Mandatory=$false, Position=0, ParameterSetName="InputWsb", HelpMessage="Path to WSB file.")]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Path,
+
+        # Path to the Sandbox file to write to. Useful to track how you last ran Windows Sandbox or to run directly with the WindowsSandbox.exe.
+        [Parameter(Mandatory=$false, ParameterSetName="BuiltWsb", HelpMessage="Output file for the built WSB XML.")]
         [ValidateScript({$_.ToLower().EndsWith(".wsb")})]
         [string]
-        $Path = "$env:Temp\Sandbox.wsb",
+        $OutFile = "$env:Temp\Sandbox.wsb",
 
+        # Hashtable mapping host paths to container paths
+        [Parameter(Mandatory=$false, ParameterSetName="BuiltWsb")]
         [ValidateScript({Approve-VolumesMap -Volumes $_})]
+        [ValidateNotNullOrEmpty()]
         [hashtable[]]
         $Volumes,
     
-        [parameter(ParameterSetName="RunCommand")]
+        # Path to a powershell script to run inside of the container
+        [Parameter(Mandatory=$false, ParameterSetName="BuiltWsb")]
         [string]
-        $RunCommand,
+        $PSScript,
     
-        [parameter(ParameterSetName="RunScript")]
-        [string]
-        $RunPSScript,
-    
-        [bool]$vGPU,
-        [bool]$Networking,
-        [bool]$ProtectedClient,
-        [bool]$AudioInput,
-        [bool]$VideoInput,
-        [bool]$PrinterRedirection,
-        [bool]$ClipboardRedirection
+        # Sandbox features to enable or disable
+        [Parameter(Mandatory=$false, ParameterSetName="BuiltWsb")]
+        [hashtable]
+        $FeatureSet
     )
+
     Process
     {
         $xml = [xml]::new()
         $xml.AppendChild($xml.CreateElement("Configuration")) | Out-Null
-        if ($null -ne $Volumes) {
-    		    $xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-    		    foreach ($Volume in $Volumes) {	
-    			      $xml.SelectSingleNode("//MappedFolders").AppendChild($xml.CreateElement("MappedFolder")) | Out-Null
-    			      $MappedFolder = $xml.SelectNodes("//MappedFolder")[$Volumes.IndexOf($Volume)]
-    			      foreach ($Child in "HostFolder", "SandboxFolder", "ReadOnly") {
-    				        $MappedFolder.AppendChild($xml.CreateElement($Child)) | Out-Null
-    			      }
-    
-    			      $MappedFolder.HostFolder = $Volume['Host']
-    			      $MappedFolder.SandboxFolder = $Volume['Container']
-    			      $MappedFolder.ReadOnly = if ($null -eq $Volume['Write']) {"True"} elseif ($Volume['Write'] -eq $true) {"False"}
-        	  }
+        if ($Volumes) {
+            $xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
+    		foreach ($Volume in $Volumes) {	
+    		    $xml.SelectSingleNode("//MappedFolders").AppendChild($xml.CreateElement("MappedFolder")) | Out-Null
+                $MappedFolder = $xml.SelectNodes("//MappedFolder")[$Volumes.IndexOf($Volume)]
+    			foreach ($Child in "HostFolder", "SandboxFolder", "ReadOnly") {
+    			    $MappedFolder.AppendChild($xml.CreateElement($Child)) | Out-Null
+    			}
+                $MappedFolder.HostFolder = $Volume['Host']
+    			$MappedFolder.SandboxFolder = $Volume['Container']
+    			$MappedFolder.ReadOnly = if ($null -eq $Volume['Write']) {"True"} elseif ($Volume['Write'] -eq $true) {"False"}
+        	}
         }
-        if ($RunCommand -ne ""){
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("LogonCommand")) | Out-Null
-    		$xml.SelectSingleNode("//LogonCommand").AppendChild($xml.CreateElement("Command")) | Out-Null
-    		$xml.Configuration.LogonCommand.Command = $RunCommand
-        }
-        if ($RunPSScript -ne "") {
+
+        if ($PSScript -ne "") {
 	    	$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("LogonCommand")) | Out-Null
     		$xml.SelectSingleNode("//LogonCommand").AppendChild($xml.CreateElement("Command")) | Out-Null
     		$EscapedScript = $RunPSScript.Replace(" ","`` ")
         	$xml.Configuration.LogonCommand.Command = "powershell.exe -ExecutionPolicy Bypass $EscapedScript"
         }
-        if ($vGPU -ne "") {
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("vGPU")) | Out-Null
-    		$xml.Configuration.vGPU = if ($vGPU) {"Enable"} else {"Disable"}
-        }
-        if ($Networking -ne "") {
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-    		$xml.Configuration.Networking = if ($Networking) {"Default"} else {"Disable"}
-        }
-        if ($ProtectedClient -ne "") {
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-    		$xml.Configuration.ProtectedClient = if ($ProtectedClient) {"Enable"} else {"Disable"}
-        }
-        if ($AudioInput -ne "") {
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-    		$xml.Configuration.AudioInput = if ($AudioInput) {"Enable"} else {"Disable"}
-        }
-        if ($VideoInput -ne "") {
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-    		$xml.Configuration.VideoInput = if ($VideoInput) {"Enable"} else {"Disable"}
-        }
-        if ($PrinterRedirection -ne "") {
-    		$xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-	    	$xml.Configuration.PrinterRedirection = if ($PrinterRedirection) {"Enable"} else {"Disable"}
-        }
-        if ($ClipboardRedirection -ne "") {
-            $xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement("MappedFolders")) | Out-Null
-            $xml.Configuration.ClipboardRedirection= if ($ClipboardRedirection) {"Default"} else {"Disable"}
+
+        foreach ($i in $FeatureSet.GetEnumerator()) {
+            $xml.SelectSingleNode("Configuration").AppendChild($xml.CreateElement($i.Name))
+            $xml.Configuration.$i.Name = if (($i.Name -eq "Networking" -and $i.Value -eq $true)) {"Default"} else {"$($i.Value)"}
         }
     
-        $xml.Save($Path)
-        WindowsSandbox.exe $Path
+        $xml.Save($OutFile)
+        WindowsSandbox.exe $OutFile
     }
 }
+
 
 New-Alias -Name sandbox -Value Start-Sandbox
 Export-ModuleMember -Function Start-Sandbox -Alias sandbox
